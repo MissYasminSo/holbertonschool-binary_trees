9f650bccd96d050e757dcbfce454a0861f8f4d8f
Yasmin So <yasmin@yasmin.so>
2026-08-17 15:02:24 +1000

    fix: not counting height correctly

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,48 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_height - height of tree from node
+ * @tree: tree to traverse
+ *
+ * Return: height of tree from node
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t lheight;
+	size_t rheight;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (0);
+	}
+
+	lheight = binary_tree_height(tree->left);
+	rheight = binary_tree_height(tree->right);
+
+	if (tree->left != NULL)
+	{
+		lheight = lheight + 1;
+	}
+
+	if (tree->right != NULL)
+	{
+		rheight = rheight + 1;
+	}
+
+	if (lheight > rheight)
+	{
+		return (lheight);
+	}
+
+	return (rheight);
+}
+
+/**
  * binary_tree_balance - gets balance factor of tree
  * @tree: tree to traverse
  *
@@ -16,8 +58,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 	}
 
-	lbalance = binary_tree_balance(tree->left);
-	rbalance = binary_tree_balance(tree->right);
+	lbalance = binary_tree_height(tree->left);
+	rbalance = binary_tree_height(tree->right);
 
 	if (tree->left != NULL)
 	{
