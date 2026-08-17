45e96bf591476ec02457d145397b217caa926043
Yasmin So <yasmin@yasmin.so>
2026-08-17 16:09:20 +1000

    fix: incorrect function call again :(

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -132,6 +132,6 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 	}
 
-	return (binary_tree_is_full(tree->left)
-		&& binary_tree_balance(tree->right) == 0);
+	return (binary_tree_is_perfect(tree->left)
+		&& binary_tree_is_perfect(tree->right));
 }
